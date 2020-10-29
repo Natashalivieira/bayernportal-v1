4d89cf987d51f7a6dbb8e4a6851069b7ab77bb00
Natasha Li Vieira <natasha.livieira@dpschool.io>
2020-10-29 17:17:41 +0100

    including an expandable button in the ReisepassCard component

--- src/Components/Verticaltabs/Secondlevel/Content/ReisepassCard.js
+++ src/Components/Verticaltabs/Secondlevel/Content/ReisepassCard.js
@@ -1,56 +1,103 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Reisepass from '../../../../Images/Reisepass.jpg'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 700,
-    height: 379,
+    maxWidth: 750,
   },
-});
-
-export default function ReisepassCard() {
-  const classes = useStyles();
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
   
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
   const bull = <span className={classes.bullet}>•</span>;
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Reisepass"
-          height="180"
-          image={Reisepass}
-          title="Reisepass"
-        />
+      <CardHeader
+        title="Reisepass"
+        subheader="Beantragung • Verlängerung"
+      />
+      <CardMedia
+        className={classes.media}
+        image={Reisepass}
+        title="Reisepass"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+        Der vorläufige Reisepass ist ein amtliches Ausweisdokument für Deutsche, der grundsätzlich zur Einreise in die Bundesrepublik Deutschland bzw. zur Ausreise aus der Bundesrepublik Deutschland benötigt wird. 
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography gutterBottom variant="h6" component="h3">
-          Reisepass {bull} Beantragung {bull} Verlängerung
+          <Typography paragraph>Beschreibung</Typography>
+          <Typography paragraph>
+          Voraussetzungen {bull} Erforderliche Unterlagen {bull} Bearbeitungsdauer {bull} Kosten {bull} Formulare
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          Der vorläufige Reisepass ist ein amtliches Ausweisdokument für Deutsche, der grundsätzlich zur Einreise in die Bundesrepublik Deutschland bzw. zur Ausreise aus der Bundesrepublik Deutschland benötigt wird. 
+          <Typography paragraph>
+          Voraussetzungen {bull} Erforderliche Unterlagen {bull} Bearbeitungsdauer {bull} Kosten {bull} Formulare
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography paragraph>
+          Voraussetzungen {bull} Erforderliche Unterlagen {bull} Bearbeitungsdauer {bull} Kosten {bull} Formulare
+          </Typography>
+          <Typography>
           Voraussetzungen {bull} Erforderliche Unterlagen {bull} Bearbeitungsdauer {bull} Kosten {bull} Formulare
           </Typography>
         </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Teilen
-        </Button>
-        <Button size="small" color="primary">
-          Weiter lesen
-        </Button>
-      </CardActions>
+      </Collapse>
     </Card>
   );
 }
